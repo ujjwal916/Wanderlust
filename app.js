150204d88ee30e7416bdfed844c9bae4d3c3dce6
Ujjwal <ujjwalb85@gmail.com>
2025-06-28 13:57:05 +0530

    Add root route to handle /

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use((req, res, next )=> {
 
 
 app.get('/', (req, res) => {
-  res.send('Wanderlust Server is Live 🚀');
+  res.redirect('/listings');
 });
 
 app.use("/listings", listingRouter);
